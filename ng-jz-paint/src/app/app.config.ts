998357ee2e2467f796d50a506dac5b14602ef48c
bkh7 <bkh7@calvin.edu>
2025-12-19 22:38:24 -0500

    added global colore pallete

--- ng-jz-paint/src/app/app.config.ts
+++ ng-jz-paint/src/app/app.config.ts
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "jz-paint", appId: "1:179701375254:web:5eb8eacd86aab037ddf037", storageBucket: "jz-paint.firebasestorage.app", apiKey: "AIzaSyA2QJUZtJ2D36m4ONVRkxSWDuIpgjDD0N8", authDomain: "jz-paint.firebaseapp.com", messagingSenderId: "179701375254", projectNumber: "179701375254", version: "2" })), provideFirestore(() => getFirestore())
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "jz-paint", appId: "1:179701375254:web:5eb8eacd86aab037ddf037", storageBucket: "jz-paint.firebasestorage.app", apiKey: "AIzaSyA2QJUZtJ2D36m4ONVRkxSWDuIpgjDD0N8", authDomain: "jz-paint.firebaseapp.com", messagingSenderId: "179701375254" })), provideFirestore(() => getFirestore())
   ]
 };
