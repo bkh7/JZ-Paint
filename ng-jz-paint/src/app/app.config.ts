a7b0e317cd85dc793bfeb77ef30a2c488ccf990d
bkh7 <bkh7@calvin.edu>
2025-12-19 19:51:56 -0500

    initialized firestore

--- ng-jz-paint/src/app/app.config.ts
+++ ng-jz-paint/src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "jz-paint", appId: "1:179701375254:web:5eb8eacd86aab037ddf037", storageBucket: "jz-paint.firebasestorage.app", apiKey: "AIzaSyA2QJUZtJ2D36m4ONVRkxSWDuIpgjDD0N8", authDomain: "jz-paint.firebaseapp.com", messagingSenderId: "179701375254", projectNumber: "179701375254", version: "2" })), provideFirestore(() => getFirestore())
   ]
 };
