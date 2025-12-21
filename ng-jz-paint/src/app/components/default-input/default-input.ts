34f3dfaf6bd8480094f3001f76a4207c35bf90d6
bkh7 <bkh7@calvin.edu>
2025-12-21 13:01:30 -0500

    implemented room card

--- ng-jz-paint/src/app/components/default-input/default-input.ts
+++ ng-jz-paint/src/app/components/default-input/default-input.ts
@@ -8,5 +8,12 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class DefaultInput {
   @Input() placeholder = 'Enter text';
+  @Input() value: string = '';
   @Output() valueChange = new EventEmitter<string>();
+
+  onInputChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.valueChange.emit(this.value);
+  }
 }
--- ng-jz-paint/src/app/components/room/room.ts
+++ ng-jz-paint/src/app/components/room/room.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { DefaultButton } from '../default-button/default-button';
 import { DefaultInput } from '../default-input/default-input';
 
@@ -10,7 +10,57 @@ import { DefaultInput } from '../default-input/default-input';
 })
 export class Room {
 
-  totalSqft: number = 0;
-  calculatedPrice: number = 0;
+  ceilingSqft: number = 0;
+  wallSqft: number = 0;
+
+  //Two-way binding variables for inputs
+  ceilingLength: string = '0';
+  ceilingWidth: string = '0';
+  wallLength: string = '0';
+  wallWidth: string = '0';
+  wallHeight: string = '0';
+
+  //Pricing
+  wallPricePerSqft: number = 0.75;
+  ceilingPricePerSqft: number = 1.00;
+
+  //Calculated prices
+  wallTotalPrice: number = 0;
+  ceilingTotalPrice: number = 0;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const rect = this.el.nativeElement.querySelector('.card').getBoundingClientRect();
+    const x = ((event.clientX - rect.left) / rect.width) * 100;
+    const y = ((event.clientY - rect.top) / rect.height) * 100;
+    this.el.nativeElement.style.setProperty('--mouse-x', `${x}%`);
+    this.el.nativeElement.style.setProperty('--mouse-y', `${y}%`);
+  }
+
+  onValueChange(){
+
+    this.calculateSqft();
+    this.calculatePrice();
+
+  }
+
+  calculateSqft() {
+
+    const ceilingArea = parseFloat(this.ceilingLength) * parseFloat(this.ceilingWidth);
+    this.ceilingSqft = ceilingArea;
+
+    const wallArea = 2 * (parseFloat(this.wallLength) + parseFloat(this.wallWidth)) * parseFloat(this.wallHeight);
+    this.wallSqft = wallArea;
+
+  }
+
+  calculatePrice() {
+
+    this.ceilingTotalPrice = this.ceilingSqft * this.ceilingPricePerSqft;
+    this.wallTotalPrice = this.wallSqft * this.wallPricePerSqft;
+
+  }
 
 }
