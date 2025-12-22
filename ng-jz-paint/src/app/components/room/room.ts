b1170ff747690947cca1aa51b7753a12a1b4d67c
bkh7 <bkh7@calvin.edu>
2025-12-22 18:20:33 -0500

    added more to room component

--- ng-jz-paint/src/app/components/room/room.ts
+++ ng-jz-paint/src/app/components/room/room.ts
@@ -12,22 +12,38 @@ export class Room {
 
   ceilingSqft: number = 0;
   wallSqft: number = 0;
+  trimSqft: number = 0;
 
   //Two-way binding variables for inputs
-  ceilingLength: string = '0';
-  ceilingWidth: string = '0';
-  wallLength: string = '0';
-  wallWidth: string = '0';
-  wallHeight: string = '0';
+  ceilingLength: string = '';
+  ceilingWidth: string = '';
+  wallLength: string = '';
+  wallWidth: string = '';
+  wallHeight: string = '';
+  trimLength: string = '';
+  trimWidth: string = '';
   roomName: string = '';
+  numDoorFaces: string = '';
+  numDoorFrames: string = '';
+  numWindowFrames: string = '';
+  numAddons: string = '0';
 
   //Pricing
   wallPricePerSqft: number = 0.75;
   ceilingPricePerSqft: number = 1.00;
-
+  trimPricePerSqft: number = 2.00;
+  pricerPerDoorFace: number = 40.00;
+  pricePerDoorFrame: number = 40.00
+  pricePerWindowFrame: number = 40.00;
   //Calculated prices
   wallTotalPrice: number = 0;
   ceilingTotalPrice: number = 0;
+  trimTotalPrice: number = 0;
+  doorFaceTotalPrice: number = 0;
+  doorFrameTotalPrice: number = 0
+  windowFrameTotalPrice: number = 0;
+  addonTotalPrice: number = 0;
+  totalPrice: number = 0;
 
   constructor(private el: ElementRef) {}
 
@@ -42,6 +58,7 @@ export class Room {
 
   onValueChange(){
 
+    this.numAddons = ((parseInt(this.numDoorFaces) || 0) + (parseInt(this.numDoorFrames) || 0) + (parseInt(this.numWindowFrames) || 0)).toString();
     this.calculateSqft();
     this.calculatePrice();
 
@@ -49,11 +66,14 @@ export class Room {
 
   calculateSqft() {
 
-    const ceilingArea = parseFloat(this.ceilingLength) * parseFloat(this.ceilingWidth);
+    const ceilingArea = (parseFloat(this.ceilingLength) || 0) * (parseFloat(this.ceilingWidth) || 0);
     this.ceilingSqft = ceilingArea;
 
-    const wallArea = 2 * (parseFloat(this.wallLength) + parseFloat(this.wallWidth)) * parseFloat(this.wallHeight);
+    const wallArea = 2 * ((parseFloat(this.wallLength) || 0) + (parseFloat(this.wallWidth) || 0)) * (parseFloat(this.wallHeight) || 0);
     this.wallSqft = wallArea;
+
+    const trimArea = (parseFloat(this.trimLength) || 0) * (parseFloat(this.trimWidth) || 0);
+    this.trimSqft = trimArea;
 
   }
 
@@ -61,7 +81,16 @@ export class Room {
 
     this.ceilingTotalPrice = this.ceilingSqft * this.ceilingPricePerSqft;
     this.wallTotalPrice = this.wallSqft * this.wallPricePerSqft;
+    this.trimTotalPrice = this.trimSqft * this.trimPricePerSqft;
 
+    this.doorFaceTotalPrice = (parseInt(this.numDoorFaces) || 0) * this.pricerPerDoorFace;
+    this.doorFrameTotalPrice = (parseInt(this.numDoorFrames) || 0) * this.pricePerDoorFrame;
+    this.windowFrameTotalPrice = (parseInt(this.numWindowFrames) || 0) * this.pricePerWindowFrame;
+    
+    this.addonTotalPrice = this.doorFaceTotalPrice + this.doorFrameTotalPrice + this.windowFrameTotalPrice;
+ 
+ 
+    this.totalPrice = this.ceilingTotalPrice + this.wallTotalPrice + this.trimTotalPrice + this.addonTotalPrice;
   }
 
 }
