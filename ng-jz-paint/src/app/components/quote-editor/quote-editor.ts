abadf155e0159c3823fcd322030c87e02819f3e7
bkh7 <bkh7@calvin.edu>
2025-12-22 19:20:14 -0500

    quote data now being sent to firestore

--- ng-jz-paint/src/app/components/quote-editor/quote-editor.ts
+++ ng-jz-paint/src/app/components/quote-editor/quote-editor.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core'; // Added QueryList, ViewChildren
 import { DefaultInput } from '../default-input/default-input';
 import { Room } from '../room/room';
-import { DefaultButton } from "../default-button/default-button";
+import { DefaultButton } from '../default-button/default-button';
+import { QuoteInterface } from '../../interfaces/quote-interface';
+import { RoomInterface } from '../../interfaces/room-interface';
+import { Firestore, collection, doc, addDoc } from '@angular/fire/firestore'; // Added for Firestore
 
 @Component({
   selector: 'quote-editor',
@@ -10,7 +13,65 @@ import { DefaultButton } from "../default-button/default-button";
   styleUrl: './quote-editor.scss',
 })
 export class QuoteEditor {
+  quoteData: QuoteInterface = {
+    quoteName: '',
+    numberOfRooms: '0',
+    totalPrice: '0',
+    rooms: [this.createEmptyRoom()], // Start with one room
+  };
 
-  quoteName: string = '';
+  @ViewChildren(Room) roomComponents!: QueryList<Room>; // Queries all <room> components
 
+  constructor(private firestore: Firestore) {} // Added constructor for Firestore
+
+  createEmptyRoom(): RoomInterface {
+    return {
+      roomName: '',
+      wallLength: '',
+      wallWidth: '',
+      wallHeight: '',
+      wallArea: '',
+      ceilingLength: '',
+      ceilingWidth: '',
+      ceilingArea: '',
+      trimLength: '',
+      trimWidth: '',
+      trimArea: '',
+      numDoorFaces: '',
+      numDoorFrames: '',
+      numWindowFrames: '',
+      numAddons: '0',
+      wallPrice: '',
+      ceilingPrice: '',
+      trimPrice: '',
+      doorFacePrice: '',
+      doorFramePrice: '',
+      windowFramePrice: '',
+      addonTotalPrice: '',
+      totalRoomPrice: '',
+    };
+  }
+
+  addRoom() {
+    this.quoteData.rooms.push(this.createEmptyRoom());
+  }
+
+  saveQuote() {
+    console.log('Saving quote...');
+    // Collect data from room components via @ViewChildren
+    const collectedRooms: RoomInterface[] = this.roomComponents.map(room => room.roomData);
+    
+    // Update quoteData
+    this.quoteData.numberOfRooms = collectedRooms.length.toString();
+    this.quoteData.totalPrice = collectedRooms.reduce((sum, room) => sum + (parseFloat(room.totalRoomPrice) || 0), 0).toString();
+    this.quoteData.rooms = collectedRooms;
+
+    // Save to Firestore v9+
+    const quotesCollection = collection(this.firestore, 'quotes');
+    addDoc(quotesCollection, this.quoteData).then(() => {
+      console.log('Quote saved');
+    }).catch(error => {
+      console.error('Error saving quote:', error);
+    });
+  }
 }
